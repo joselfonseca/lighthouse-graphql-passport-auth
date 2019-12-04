340108df917c222021ec075dd9102e5abf319e5c
Koen Lavrijssen <k.lavrijssen@gmail.com>
2019-12-04 12:10:55 +0100

    Revert "master: Get args from input"
    This reverts commit 91c9112f10c22ce3feb01dbaadb5c40af3084117.

--- src/GraphQL/Mutations/Login.php
+++ src/GraphQL/Mutations/Login.php
@@ -20,8 +20,9 @@ class Login extends BaseAuthResolver
         $credentials = $this->buildCredentials($args);
         $response = $this->makeRequest($credentials);
         $model = app(config('auth.providers.users.model'));
-        $user = $model->where(config('lighthouse-graphql-passport.username'), $credentials['username'])->firstOrFail();
+        $user = $model->where(config('lighthouse-graphql-passport.username'), $args['username'])->firstOrFail();
         $response['user'] = $user;
         return $response;
     }
+
 }
