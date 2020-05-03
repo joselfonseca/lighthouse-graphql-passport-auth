49c25f4e220ba0b33626236909d9aff81b2ee106
Jose Luis Fonseca <joselfonseca@users.noreply.github.com>
2020-05-03 07:43:11 -0500

    use Laravel Helper

--- src/GraphQL/Mutations/BaseAuthResolver.php
+++ src/GraphQL/Mutations/BaseAuthResolver.php
@@ -42,7 +42,7 @@ class BaseAuthResolver
         $response = app()->handle($request);
         $decodedResponse = json_decode($response->getContent(), true);
         if ($response->getStatusCode() != 200) {
-            throw new AuthenticationException(_('Authentication exception'), _('Incorrect username or password'));
+            throw new AuthenticationException(__('Authentication exception'), __('Incorrect username or password'));
         }
 
         return $decodedResponse;
