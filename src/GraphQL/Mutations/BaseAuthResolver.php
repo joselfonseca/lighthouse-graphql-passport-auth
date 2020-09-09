ac42a9d1f1a59944fd50b2a77c9f560828991d60
joselfonseca <jose.fonseca@domandtom.com>
2020-09-09 08:03:31 -0500

    use passport messages

--- src/GraphQL/Mutations/BaseAuthResolver.php
+++ src/GraphQL/Mutations/BaseAuthResolver.php
@@ -43,7 +43,10 @@ class BaseAuthResolver
         $response = app()->handle($request);
         $decodedResponse = json_decode($response->getContent(), true);
         if ($response->getStatusCode() != 200) {
-            throw new AuthenticationException(__('Authentication exception'), __('Incorrect username or password'));
+            if ($decodedResponse['message'] === "The provided authorization grant (e.g., authorization code, resource owner credentials) or refresh token is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.") {
+                throw new AuthenticationException(__('Authentication exception'), __('Incorrect username or password'));
+            }
+            throw new AuthenticationException(__($decodedResponse['error']), __($decodedResponse['message']));
         }
 
         return $decodedResponse;
--- tests/Integration/GraphQL/Mutations/LoginTest.php
+++ tests/Integration/GraphQL/Mutations/LoginTest.php
@@ -88,4 +88,115 @@ class LoginTest extends TestCase
             return $event->user->id === $user->id;
         });
     }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test_it_returns_error_for_invalid_credentials(string $modelClass, array $credentials, bool $hasFindForPassportMethod = false)
+    {
+        Event::fake([UserLoggedIn::class]);
+
+        $this->app['config']->set('auth.providers.users.model', $modelClass);
+
+        $this->createClient();
+
+        $user = factory($modelClass)->create();
+
+        if ($hasFindForPassportMethod) {
+            self::assertTrue(method_exists($modelClass, 'findForPassport'));
+        }
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation Login($input: LoginInput) {
+                login(input: $input) {
+                    access_token
+                    refresh_token
+                    user {
+                        id
+                        name
+                        email
+                    }
+                }
+            }
+        ',
+            [
+                'input' => [
+                    'username' => 'something',
+                    'password' => 'somethingelse'
+                ],
+            ]
+        );
+
+        $response->assertJsonStructure([
+            'errors' => [
+                [
+                    'message',
+                    'extensions' => [
+                        'category',
+                        'reason'
+                    ]
+                ]
+            ],
+        ]);
+
+        $decodedResponse = json_decode($response->getContent(), 'true');
+
+        $this->assertEquals('Incorrect username or password', $decodedResponse['errors'][0]['extensions']['reason']);
+
+        Event::assertNotDispatched(UserLoggedIn::class);
+
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test_it_returns_correct_error_for_client(string $modelClass, array $credentials, bool $hasFindForPassportMethod = false)
+    {
+
+        $this->artisan('migrate', ['--database' => 'testbench']);
+
+        Event::fake([UserLoggedIn::class]);
+
+        $this->app['config']->set('auth.providers.users.model', $modelClass);
+
+        if ($hasFindForPassportMethod) {
+            self::assertTrue(method_exists($modelClass, 'findForPassport'));
+        }
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation Login($input: LoginInput) {
+                login(input: $input) {
+                    access_token
+                    refresh_token
+                    user {
+                        id
+                        name
+                        email
+                    }
+                }
+            }
+        ',
+            [
+                'input' => $credentials,
+            ]
+        );
+
+        $response->assertJsonStructure([
+            'errors' => [
+                [
+                    'message',
+                    'extensions' => [
+                        'category',
+                        'reason'
+                    ]
+                ]
+            ],
+        ]);
+
+        $decodedResponse = json_decode($response->getContent(), 'true');
+
+        $this->assertEquals('invalid_client', $decodedResponse['errors'][0]['message']);
+
+        Event::assertNotDispatched(UserLoggedIn::class);
+    }
 }
