10f54930f53223ad03b164e98b341d4286946036
wimski <wimreckman@gmail.com>
2020-10-12 16:57:57 +0200

    Extract token from verificationUrl in VerifyEmail

--- src/Notifications/VerifyEmail.php
+++ src/Notifications/VerifyEmail.php
@@ -15,12 +15,24 @@ class VerifyEmail extends \Illuminate\Auth\Notifications\VerifyEmail
      */
     protected function verificationUrl($notifiable)
     {
-        $payload = base64_encode(json_encode([
+        $payload = $this->getToken($notifiable);
+
+        return config('lighthouse-graphql-passport.verify_email.base_url').'?token='.$payload;
+    }
+
+    /**
+     * Get a token for the given notifiable.
+     *
+     * @param mixed $notifiable
+     *
+     * @return string
+     */
+    protected function getToken($notifiable)
+    {
+        return base64_encode(json_encode([
             'id'         => $notifiable->getKey(),
             'hash'       => encrypt($notifiable->getEmailForVerification()),
             'expiration' => encrypt(Carbon::now()->addMinutes(10)->toIso8601String()),
         ]));
-
-        return config('lighthouse-graphql-passport.verify_email.base_url').'?token='.$payload;
     }
 }
