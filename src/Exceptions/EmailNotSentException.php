a9dc8ff3482afc2a6156bc9888fbae93e3249690
joselfonseca <jose.fonseca@domandtom.com>
2020-12-30 07:53:10 -0500

    throw exception if could not send email

---
+++ src/Exceptions/EmailNotSentException.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Joselfonseca\LighthouseGraphQLPassport\Exceptions;
+
+use Exception;
+use Nuwave\Lighthouse\Exceptions\RendersErrorsExtensions;
+
+class EmailNotSentException extends Exception implements RendersErrorsExtensions
+{
+    /**
+     * @var @string
+     */
+    private $reason;
+
+    public function __construct(string $message, string $reason)
+    {
+        parent::__construct($message);
+
+        $this->reason = $reason;
+    }
+
+    /**
+     * Returns true when exception message is safe to be displayed to a client.
+     *
+     * @api
+     *
+     * @return bool
+     */
+    public function isClientSafe(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns string describing a category of the error.
+     *
+     * Value "graphql" is reserved for errors produced by query parsing or validation, do not use it.
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getCategory(): string
+    {
+        return 'notifications';
+    }
+
+    /**
+     * Return the content that is put in the "extensions" part
+     * of the returned error.
+     *
+     * @return array
+     */
+    public function extensionsContent(): array
+    {
+        return [
+            'reason' => $this->reason,
+        ];
+    }
+}
--- src/GraphQL/Mutations/ForgotPassword.php
+++ src/GraphQL/Mutations/ForgotPassword.php
@@ -6,6 +6,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Support\Facades\Password;
 use Joselfonseca\LighthouseGraphQLPassport\Events\ForgotPasswordRequested;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Joselfonseca\LighthouseGraphQLPassport\Exceptions\EmailNotSentException;
 
 class ForgotPassword
 {
@@ -22,6 +23,7 @@ class ForgotPassword
     public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
         $response = $this->broker()->sendResetLink(['email' => $args['email']]);
+
         if ($response == Password::RESET_LINK_SENT) {
             event(new ForgotPasswordRequested($args['email']));
 
@@ -31,10 +33,7 @@ class ForgotPassword
             ];
         }
 
-        return [
-            'status'  => 'EMAIL_NOT_SENT',
-            'message' => __($response),
-        ];
+        throw new EmailNotSentException('Email not sent', __($response));
     }
 
     /**
