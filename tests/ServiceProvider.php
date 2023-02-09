1e20ffa33a8e8c2100967f1f182fa13635f2088e
Bhushan Mer <bhushanmer@gmail.com>
2023-02-09 11:00:36 +0530

    Code format

--- tests/ServiceProvider.php
+++ tests/ServiceProvider.php
@@ -9,7 +9,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(realpath(__DIR__.'/../tests/migrations'));
-        if(method_exists(Passport::class, 'routes')) {
+        if (method_exists(Passport::class, 'routes')) {
             Passport::routes();
         }
         Passport::loadKeysFrom(__DIR__.'/storage/');
